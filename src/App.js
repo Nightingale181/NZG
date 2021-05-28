5ca08d33aabd0632358ffae80d1f77568651fa2d
Nightingale181 <vgalyts@gmail.com>
2021-05-28 15:53:48 +0300

    build fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Client} from "boardgame.io/react";
+// import { Local } from "boardgame.io/multiplayer"; //TODO: Multiplayer
+import NZGBoard from "./NZGBoard"
+import {Game} from "./Game.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const GameClient = Client({
+  game: Game,
+  board: NZGBoard,
+  numPlayers: 4,
+  debug: true,
+
+});
+const App = () => (
+    <div>
+      <GameClient/>
     </div>
-  );
-}
 
+);
 export default App;
+
+
