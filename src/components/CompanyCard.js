a7c3ce301105a1a8a7ab48d5b08c4846da3c7185
Nightingale181 <vgalyts@gmail.com>
2021-09-13 12:50:04 +0300

    test svg zoom one card while card dealing in progress

--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -26,7 +26,7 @@ const useStyles =  makeStyles(theme => (
     },
 
     backImage: card =>( {
-        backgroundImage:`url(${process.env.PUBLIC_URL + '/img/testsvg2.svg'})`,
+        backgroundImage:`url(${process.env.PUBLIC_URL + '/img/airline.svg'})`,
         backgroundPosition: 'center',
         backgroundSize: '100%',
         backgroundRepeat: 'no-repeat',
